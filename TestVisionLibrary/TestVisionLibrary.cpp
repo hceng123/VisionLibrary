baccf99da6a19e4361a40a1330b1aad76228938b
Xiao Shengguang <shengguangxiao@gmail.com>
2017-01-11 16:00:04 +0800

    Put all the build generated files into obj folder
    Put all the build generated files into obj folder

--- TestVisionLibrary/TestVisionLibrary.cpp
+++ TestVisionLibrary/TestVisionLibrary.cpp
@@ -4,8 +4,8 @@
 #include "stdafx.h"
 #include "../VisionLibrary/VisionAPI.h"
 #include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 #include <ctime>
 #include <iostream>
 
