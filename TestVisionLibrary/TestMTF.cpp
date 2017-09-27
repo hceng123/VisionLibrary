369f064e79a15317db7e3269a46993ea9f32c32f
Xiao Shengguang <shengguangxiao@gmail.com>
2017-09-27 18:35:28 +0800

    Add TestMTF.cpp file.
    Add TestMTF.cpp file.

---
+++ TestVisionLibrary/TestMTF.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "../VisionLibrary/VisionAPI.h"
+#include "opencv2/highgui.hpp"
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include "TestSub.h"
+
+using namespace AOI::Vision;
+
+void TestCalcCameraMTF() {
+    PR_CALC_CAMERA_MTF_CMD stCmd;
+    PR_CALC_CAMERA_MTF_RPY stRpy;
+}
