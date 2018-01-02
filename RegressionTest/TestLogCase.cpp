0020aa3eb7a221d3bdfea5d00f46620149e16ce1
Xiao Shengguang <shengguangxiao@gmail.com>
2018-01-02 16:19:44 +0800

    Update TestRunLogcase function.
    Update TestRunLogcase function, make regression test doesn't change the
    original logcase file.

--- RegressionTest/TestLogCase.cpp
+++ RegressionTest/TestLogCase.cpp
@@ -3,6 +3,9 @@
 #include "opencv2/imgproc.hpp"
 #include "../VisionLibrary/VisionAPI.h"
 #include <iostream>
+#include "boost/filesystem.hpp"
+
+namespace bfs = boost::filesystem;
 
 namespace AOI
 {
@@ -16,12 +19,18 @@ void TestRunLogcase()
     std::cout << std::endl << "------------------------------------------";
     std::cout << std::endl;
 
+    std::string strInputLogCase(".\\Logcase\\AutoThreshold_Success.logcase");
+    std::string strCloneLogCase(".\\Logcase\\AutoThreshold_Clone.logcase");
+    bfs::copy_file ( strInputLogCase, strCloneLogCase );
+
     VisionStatus enStatus = VisionStatus::OK;
-    enStatus = PR_RunLogCase(".\\Logcase\\AutoThreshold_Success.logcase");
+    enStatus = PR_RunLogCase(".\\Logcase\\AutoThreshold_Clone.logcase");
     std::cout << "Run logcase result " << static_cast<int> ( enStatus ) << std::endl;
 
-    enStatus = PR_RunLogCase("./Logcase/AutoThreshold_Success.logcase");
+    enStatus = PR_RunLogCase("./Logcase/AutoThreshold_Clone.logcase");
     std::cout << "Run logcase result " << static_cast<int> ( enStatus ) << std::endl;
+
+    bfs::remove ( strCloneLogCase );
 }
 
 }
